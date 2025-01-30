2492570fd0e0b348ab11dea580e15bfd2425d295
Robert Wypiór <robwypior@gmail.com>
2025-01-30 19:22:13 +0100

    Remove editor project; Change to single-header library

---
+++ include/wxmarkdownctrl/wxMarkdownCtrl.h
@@ -0,0 +1,58 @@
+﻿//#pragma once
+//
+//#include "cppmarkdown/cppMarkdown.h"
+//
+//#include <wx/wx.h>
+//#include <wx/wxhtml.h>
+//
+//class MarkdownCtrl : public wxHtmlWindow
+//{
+//public:
+//    MarkdownCtrl();
+//
+//    MarkdownCtrl(wxWindow* parent,
+//        wxWindowID id,
+//        const wxPoint& pos = wxDefaultPosition,
+//        const wxSize& size = wxDefaultSize,
+//        long style = 0,
+//        const wxString& name = wxASCII_STR(wxPanelNameStr));
+//
+//    void setMarkdown(const wxString &markdown);
+//    void refreshMarkdown();
+//
+//private:
+//    Markdown::Document markdownDocument;
+//};
+
+#pragma once
+
+#include "cppmarkdown/cppMarkdown.h"
+
+#include <wx/wx.h>
+#include <wx/wxhtml.h>
+
+class MarkdownCtrl : public wxHtmlWindow
+{
+public:
+    using wxHtmlWindow::wxHtmlWindow;
+
+    void setMarkdown(const wxString& markdown)
+    {
+        this->markdownDocument.clear();
+        this->markdownDocument.parse(markdown.ToStdString());
+        this->refreshMarkdown();
+    }
+
+    void refreshMarkdown()
+    {
+        this->SetPage(this->markdownDocument.getHtml());
+    }
+
+    Markdown::Document& getMarkdownDocument()
+    {
+        return this->markdownDocument;
+    }
+
+private:
+    Markdown::Document markdownDocument;
+};
