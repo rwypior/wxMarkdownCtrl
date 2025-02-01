b4ffadccf9bc07ac2dc41bb1c68086a0763c33fc
Robert <robwypior@gmail.com>
2025-02-01 16:14:59 +0100

    Cleanup

--- include/wxmarkdownctrl/wxMarkdownCtrl.h
+++ include/wxmarkdownctrl/wxMarkdownCtrl.h
@@ -1,32 +1,6 @@
-﻿//#pragma once
-//
-//#include "cppmarkdown/cppMarkdown.h"
-//
-//#include <wx/wx.h>
-//#include <wx/wxhtml.h>
-//
-//class MarkdownCtrl : public wxHtmlWindow
-//{
-//public:
-//    MarkdownCtrl();
-//
-//    MarkdownCtrl(wxWindow* parent,
-//        wxWindowID id,
-//        const wxPoint& pos = wxDefaultPosition,
-//        const wxSize& size = wxDefaultSize,
-//        long style = 0,
-//        const wxString& name = wxASCII_STR(wxPanelNameStr));
-//
-//    void setMarkdown(const wxString &markdown);
-//    void refreshMarkdown();
-//
-//private:
-//    Markdown::Document markdownDocument;
-//};
-
 #pragma once
 
-#include "cppmarkdown/cppMarkdown.h"
+#include "cppmarkdown/cppmarkdown.h"
 
 #include <wx/wx.h>
 #include <wx/wxhtml.h>
